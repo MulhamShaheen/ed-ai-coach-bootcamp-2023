b2fef6553ad5336c0828a48beb48d670efc3c612
Alexandr Utkov <alex.com195@gmail.com>
2023-09-08 13:18:02 +0000

    Added background analysis

--- mediapipe_demo.py
+++ mediapipe_demo.py
@@ -25,9 +25,9 @@ MODELS_ROOT = os.path.join(SCRIPT_DIR, "models")
 SAMPLE_IMAGES_ROOT = os.path.join(SCRIPT_DIR, "demo_images")
 HAND_MODEL_PATH = os.path.join(MODELS_ROOT, "hand_landmarker.task")
 FACE_MODEL_PATH = os.path.join(MODELS_ROOT, "face_landmarker.task")
+SEGMENT_MODEL_PATH = os.path.join(MODELS_ROOT, "selfie_segmenter.tflite")
 HAND_IMG_SAMPLE = os.path.join(SAMPLE_IMAGES_ROOT, "hand_image.jpg")
 FACE_SAMPLE = os.path.join(SAMPLE_IMAGES_ROOT, "face_image.jpg")
-VIDEO_PATH = os.path.join(SAMPLE_IMAGES_ROOT, "test_video.mp4")
 
 
 class ModelsFactory:
@@ -60,6 +60,16 @@ class ModelsFactory:
         pos_net.eval()
 
         return pos_net
+    
+    @staticmethod
+    def init_segmentation_model()-> vision.ImageSegmenter:
+        base_options = python.BaseOptions(model_asset_path=SEGMENT_MODEL_PATH)
+        options = vision.ImageSegmenterOptions(
+            base_options=base_options,
+            output_category_mask=True
+        )
+        segmenter = vision.ImageSegmenter.create_from_options(options)
+        return segmenter
 
 
 class VizUtils:  
@@ -187,66 +197,81 @@ class Utils:
         roll_predicted = torch.sum(roll_predicted.data * idx_tensor) * 3 - 99
 
         return yaw_predicted, pitch_predicted, roll_predicted
+    
+    @staticmethod
+    def count_background_std(category_mask, orig_img_npy):
+        category_mask = category_mask.numpy_view()
+        category_mask = category_mask[:, :, np.newaxis].copy() 
+        category_mask = np.repeat(category_mask, 3, axis=2)
+
+        bool_mask = np.array(category_mask, dtype=bool)
+        bool_mask_flatten = bool_mask[:,:,0].flatten()
+
+        result = bool_mask * orig_img_npy
+
+        res = []
+        for chanel in range(3):
+            flat_chan = result[:,:,chanel].flatten()
+            flat_chan = flat_chan * bool_mask_flatten
+            res.append(np.std(flat_chan))
+        return np.ndarray(res)
+
+
+# def mediapipe_hands_inference_demo() -> None:
+#     base_options = python.BaseOptions(model_asset_path=HAND_MODEL_PATH)
+#     options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
+#     detector = vision.HandLandmarker.create_from_options(options)
+
+#     # STEP 3: Load the input image.
+#     image = mp.Image.create_from_file(HAND_IMG_SAMPLE)
+#     # STEP 4: Detect hand landmarks from the input image.
+#     detection_result: HandLandmarkerResult = detector.detect(image)
+#     pprint(detection_result)
+#     viz_image = cv2.imread(HAND_IMG_SAMPLE)
+#     viz_image = VizUtils.draw_hand_detection_result(viz_image, result=detection_result)
+#     cv2.imshow("demo", viz_image)
+#     cv2.waitKey(0)
+
+
+# def mediapipe_face_inference_demo() -> None:
+#     base_options = python.BaseOptions(model_asset_path=FACE_MODEL_PATH)
+#     options = vision.FaceLandmarkerOptions(
+#         base_options=base_options,
+#         output_face_blendshapes=True,
+#         output_facial_transformation_matrixes=True,
+#         num_faces=1,
+#     )
+#     detector = vision.FaceLandmarker.create_from_options(options)
+
+#     # STEP 3: Load the input image.
+#     image = mp.Image.create_from_file(FACE_SAMPLE)
+#     viz_image = cv2.imread(FACE_SAMPLE)
+#     # STEP 4: Detect face landmarks from the input image.
+#     face_detection_result: FaceLandmarkerResult = detector.detect(image)
+
+#     with open('face_detection_result.pkl', 'wb') as f:
+#         pickle.dump(face_detection_result, f)
+
+#     face = face_detection_result.face_landmarks[0]
+#     for point in face:
+#         viz_image = VizUtils.draw_landmark(viz_image, point)
+#     cv2.imwrite('result.jpg', viz_image)
+#     # cv2.imshow("Face Detection", viz_image)
+#     # cv2.waitKey(0)
 
 
 
-def mediapipe_hands_inference_demo() -> None:
-    base_options = python.BaseOptions(model_asset_path=HAND_MODEL_PATH)
-    options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
-    detector = vision.HandLandmarker.create_from_options(options)
+def mediapipe_save_video_demo(NAME) -> None:
+    VIDEO_PATH = os.path.join(SAMPLE_IMAGES_ROOT, f"{NAME}.mp4")
 
-    # STEP 3: Load the input image.
-    image = mp.Image.create_from_file(HAND_IMG_SAMPLE)
-    # STEP 4: Detect hand landmarks from the input image.
-    detection_result: HandLandmarkerResult = detector.detect(image)
-    pprint(detection_result)
-    viz_image = cv2.imread(HAND_IMG_SAMPLE)
-    viz_image = VizUtils.draw_hand_detection_result(viz_image, result=detection_result)
-    cv2.imshow("demo", viz_image)
-    cv2.waitKey(0)
+    std_list = []
+    direction_list = []
 
 
-def mediapipe_face_inference_demo() -> None:
-    base_options = python.BaseOptions(model_asset_path=FACE_MODEL_PATH)
-    options = vision.FaceLandmarkerOptions(
-        base_options=base_options,
-        output_face_blendshapes=True,
-        output_facial_transformation_matrixes=True,
-        num_faces=1,
-    )
-    detector = vision.FaceLandmarker.create_from_options(options)
-
-    # STEP 3: Load the input image.
-    image = mp.Image.create_from_file(FACE_SAMPLE)
-    viz_image = cv2.imread(FACE_SAMPLE)
-    # STEP 4: Detect face landmarks from the input image.
-    face_detection_result: FaceLandmarkerResult = detector.detect(image)
-
-    with open('face_detection_result.pkl', 'wb') as f:
-        pickle.dump(face_detection_result, f)
-
-    face = face_detection_result.face_landmarks[0]
-    for point in face:
-        viz_image = VizUtils.draw_landmark(viz_image, point)
-    cv2.imwrite('result.jpg', viz_image)
-    # cv2.imshow("Face Detection", viz_image)
-    # cv2.waitKey(0)
-
-
-def mediapipe_save_video_demo() -> None:
-    vid = cv2.VideoCapture(0)
     hand_detector = ModelsFactory.init_hand_model()
     face_detector = ModelsFactory.init_face_model()
     direction_detector = ModelsFactory.init_direction_model()
-
-
-    base_options = python.BaseOptions(model_asset_path=FACE_MODEL_PATH)
-    options = vision.FaceLandmarkerOptions(
-        base_options=base_options,
-        output_face_blendshapes=True,
-        output_facial_transformation_matrixes=True,
-        num_faces=1,
-    )
+    segmenter = ModelsFactory.init_segmentation_model()
 
     video = cv2.VideoCapture(VIDEO_PATH)
     frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -257,24 +282,28 @@ def mediapipe_save_video_demo() -> None:
 
         rgb_frame = cv2.cvtColor(frame_cv2, cv2.COLOR_BGR2RGB)
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+        
         hand_detection_result: HandLandmarkerResult = hand_detector.detect(mp_image)
         face_detection_result: FaceLandmarkerResult = face_detector.detect(mp_image)
+        segmentation_result = segmenter.segment(mp_image)
+
         if len(face_detection_result.face_landmarks) != 0:
             bbox_data = Utils.get_bbox_coords(frame_cv2, face_detection_result)
             img_tensor = Utils.preprocess_direction_input(frame_cv2, bbox_data)
-
             yaw, pitch, roll = direction_detector(img_tensor)
             yaw, pitch, roll = Utils.postprocess_direction_output(yaw, pitch, roll)
+            direction_list.append(np.ndarray((yaw, pitch, roll)))
+            
+            rgb_std = Utils.count_background_std(segmentation_result.category_mask, rgb_frame)
+            std_list.append(rgb_std)
+
 
             rgb_frame = VizUtils.draw_axis(rgb_frame, yaw, pitch, roll,
                                            bbox_data['tdx'], bbox_data['tdy'], size=200)
-            
             rgb_frame = VizUtils.draw_hand_detection_result(
                 rgb_frame, result=hand_detection_result
             )
-
             frame_cv2 = cv2.cvtColor(rgb_frame, cv2.COLOR_RGB2BGR)
-
             frame_cv2 = cv2.rectangle(frame_cv2, (bbox_data['x_left'], bbox_data['y_top']),
                                     (bbox_data['x_right'], bbox_data['y_bottom']),
                                     (0,255,0), 2)
@@ -291,6 +320,11 @@ def mediapipe_save_video_demo() -> None:
                                   audio_codec='aac',
                                   verbose=False,
                                   logger=None)
+    
+    direction_np = np.stack(direction_list, axis=0)
+    std_np = np.stack(std_list, axis=0)
+    np.savez(f'{NAME}.npz', direction_np=direction_np, std_np=std_np)
+
 
 
 def articulation_detection() -> bool:
@@ -323,5 +357,8 @@ def webcam_demo() -> None:
 
 
 if __name__ == "__main__":
-    mediapipe_save_video_demo()
+    name = 'Макаров'
+    mediapipe_save_video_demo(name)
+
+    # mediapipe_mask_inference_demo()
     
